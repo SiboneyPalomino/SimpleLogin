54e056bd8483a045a8dfad3c313a4ae52df8704b
siboney.rios <>
2022-08-29 14:49:08 -0500

    Init project

---
+++ SimpleLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleLogin
+//
+//  Created by siboney.rios on 28/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
